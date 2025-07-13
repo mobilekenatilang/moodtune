377bb97adfc7bde272ddf1e5de34160262b4957d
Hyvos <daniel.liman07@gmail.com>
2025-07-14 02:08:12 +0700

    feat: Floating navbar

--- lib/app_wrapper.dart
+++ lib/app_wrapper.dart
@@ -1,25 +1,166 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:moodtune/core/themes/_themes.dart';
+import 'package:moodtune/core/widgets/navbar_item.dart';
+import 'package:moodtune/features/home/presentation/pages/homepage.dart';
+import 'package:moodtune/services/logger_service.dart';
+import 'dart:ui';
 
-class AppWrapper extends StatefulWidget {
+class AppWrapper extends StatelessWidget {
   const AppWrapper({super.key});
 
   @override
-  State<AppWrapper> createState() => _AppWrapperState();
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => NavigationCubit(),
+      child: const NavigationMenu(),
+    );
+  }
 }
 
-class _AppWrapperState extends State<AppWrapper> {
-  // TODO: Build navbar here
+class NavigationMenu extends StatelessWidget {
+  const NavigationMenu({super.key});
+
+  // List of pages to navigate between
+  static const List<Widget> _pages = [
+    HomePage(),
+    ExamplePage(title: "Status"),
+    ExamplePage(title: "Playlist"),
+    ExamplePage(title: "Profile"),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Welcome to MoodTune!',
-          style: FontTheme.poppins24w700black(),
-        ),
+      body: Stack(
+        children: [
+          SafeArea(
+            child: BlocBuilder<NavigationCubit, int>(
+              builder: (context, selectedIndex) {
+                return _pages[selectedIndex];
+              },
+            ),
+          ),
+          // Non-clickable overlay
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            height: 80,
+            child: IgnorePointer(
+              ignoring: false,
+              child: Container(color: Colors.transparent),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 50,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.transparent,
+                    BaseColors.neutral100.withValues(alpha: 0.05),
+                    BaseColors.neutral100.withValues(alpha: 0.1),
+                    BaseColors.neutral100.withValues(alpha: 0.2),
+                    BaseColors.neutral100.withValues(alpha: 0.4),
+                  ],
+                  stops: const [0.0, 0.3, 0.5, 0.7, 1.0],
+                ),
+              ),
+              child: ClipRRect(
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 0.75, sigmaY: 0.75),
+                  child: Container(color: Colors.transparent),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 24,
+            left: 0,
+            right: 0,
+            child: BlocBuilder<NavigationCubit, int>(
+              builder: (context, selectedIndex) {
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 280,
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: BaseColors.white,
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(48),
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: navbarItems
+                            .map(
+                              (item) => NavItem(
+                                item: item,
+                                isSelected:
+                                    navbarItems.indexOf(item) == selectedIndex,
+                                onTap: () {
+                                  context.read<NavigationCubit>().changeTab(
+                                    navbarItems.indexOf(item),
+                                  );
+                                },
+                              ),
+                            )
+                            .toList(),
+                      ),
+                    ),
+                    const SizedBox(width: 6),
+                    Container(
+                      width: 52,
+                      height: 52,
+                      decoration: BoxDecoration(
+                        color: BaseColors.gold3,
+                        shape: BoxShape.circle,
+                      ),
+                      child: IconButton(
+                        onPressed: () {
+                          LoggerService.i('Add button pressed');
+                        },
+                        icon: const Icon(
+                          Icons.add,
+                          color: BaseColors.white,
+                          size: 24,
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
+  }
+}
+
+class NavigationCubit extends Cubit<int> {
+  NavigationCubit() : super(0);
+
+  void changeTab(int index) {
+    emit(index);
+  }
+}
+
+// NOTE: Placeholder pages - replace this ASAP
+class ExamplePage extends StatelessWidget {
+  final String title;
+  const ExamplePage({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text(title, style: FontTheme.poppins24w700black()));
   }
 }
