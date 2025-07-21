69f60f77833c09e67322b0527ef201bd3b3741f4
Hyvos <daniel.liman07@gmail.com>
2025-07-22 03:23:50 +0700

    core: New endpoints config, time generator method, and showing a snackbar function

--- lib/core/environments/endpoints.dart
+++ lib/core/environments/endpoints.dart
@@ -5,5 +5,11 @@ class EndPoints {
   static const String baseUrl = Config.baseUrlHp;
 
   static const String example = '$baseUrl/example';
+  static const String analyze = '$baseUrl/api/analyze';
+  static const String llm = '$baseUrl/api/llm';
+  static const String musicSearch = '$baseUrl/api/music/search';
+  static const String dummyData = '$baseUrl/api/dummy';
+
+  // Khusus Quote API
   static const String quote = 'https://zenquotes.io/api/today';
 }
