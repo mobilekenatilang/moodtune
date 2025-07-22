6f8f436b1b304fe2622ff85b3a12651f8fb2b9cc
Hyvos <daniel.liman07@gmail.com>
2025-07-22 17:25:48 +0700

    fix: reduce lag by eliminating that blurry effect

--- lib/features/journal/presentation/pages/_pages.dart
+++ lib/features/journal/presentation/pages/_pages.dart
@@ -1,7 +1,4 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart' hide TimeOfDay;
-import 'package:logger/web.dart';
 import 'package:lucide_icons/lucide_icons.dart';
 import 'package:moodtune/app.dart';
 import 'package:moodtune/core/extensions/_extensions.dart';
@@ -9,7 +6,6 @@ import 'package:moodtune/core/themes/_themes.dart';
 import 'package:moodtune/core/utils/date_generator.dart';
 import 'package:moodtune/core/utils/show_snackbar.dart';
 import 'package:moodtune/features/home/presentation/cubit/_cubit.dart';
-import 'package:moodtune/features/home/presentation/pages/_pages.dart';
 import 'package:moodtune/features/journal/data/model/journal.dart';
 import 'package:moodtune/features/journal/domain/usecases/_usecases.dart';
 import 'package:moodtune/services/dependencies/di.dart';
